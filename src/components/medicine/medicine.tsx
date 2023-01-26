b05373953ddd7412aa91e9f79304574ac55dd418
mohamadreza <mohamadrezakhf@Gmail.com>
2023-01-27 02:20:32 +0330

    [feature] medicine and patient component

--- src/components/medicine/medicine.tsx
+++ src/components/medicine/medicine.tsx
@@ -1,7 +1,124 @@
-import * as React from "react"
+import { Button } from "@mui/material"
+import Container from "@mui/material/Container"
+import TextField from "@mui/material/TextField"
+import { useState } from "react"
+import styles from "./styles.module.css"
+import React from "react"
+function Medicine() {
+	const [name, setName] = useState()
+	const [adminstration, setAdminstration] = useState()
+	const [productionDate, setProductionDate] = useState()
+	const [expiryDate, setExpireDate] = useState()
+	const [ageLimit, setAgeLimit] = useState()
+	const [company, setCompany] = useState()
 
-function Medicine(props: any) {
-	return <span>medicine funcional component</span>
+	function handleChangeName(event: any) {
+		setName(event.target.value)
+	}
+
+	function handleChangeAdminstration(event: any) {
+		setAdminstration(event.target.value)
+	}
+
+	function handleChangeProductionDate(event: any) {
+		setProductionDate(event.target.value)
+	}
+
+	function handleChangeExpiryDate(event: any) {
+		setExpireDate(event.target.value)
+	}
+
+	function handleChangeAgeLimit(event: any) {
+		setAgeLimit(event.target.value)
+	}
+
+	function handleChangeCompany(event: any) {
+		setCompany(event.target.value)
+	}
+
+	function createMedicine() {}
+
+	return (
+		<Container fixed sx={{ bgcolor: "#FFFFFF", height: "70vh" }}>
+			<div className={styles.makeToColumn}>
+				<div className={styles.makeToRow}>
+					<TextField
+						helperText="Enter medicine name"
+						required
+						value={name}
+						onChange={handleChangeName}
+						id="outlined-basic"
+						label="name"
+						variant="outlined"
+					/>
+					<TextField
+						required
+						helperText="Enter medicine adminstration"
+						onChange={handleChangeAdminstration}
+						value={adminstration}
+						id="outlined-basic"
+						label="adminstration"
+						variant="outlined"
+					/>
+				</div>
+				<div className={styles.makeToRow}>
+					<TextField
+						required
+						helperText="Enter production date of medicine"
+						value={productionDate}
+						onChange={handleChangeProductionDate}
+						id="outlined-basic"
+						label="production date"
+						variant="outlined"
+					/>
+					<TextField
+						helperText="Enter expiry date of medicine"
+						required
+						value={expiryDate}
+						onChange={handleChangeExpiryDate}
+						id="outlined-basic"
+						label="expiry date"
+						variant="outlined"
+					/>
+				</div>
+				<div className={styles.makeToRow}>
+					<TextField
+						required
+						value={ageLimit}
+						helperText="Enter age limit of medicine"
+						onChange={handleChangeAgeLimit}
+						id="outlined-basic"
+						label="age limit"
+						variant="outlined"
+					/>
+					<TextField
+						helperText="Enter company of medicine"
+						required
+						value={company}
+						onChange={handleChangeCompany}
+						id="outlined-basic"
+						label="company"
+						variant="outlined"
+					/>
+				</div>
+				<div className={styles.buttonDiv}>
+					<Button
+						variant="contained"
+						onClick={createMedicine}
+						type="submit"
+						sx={{
+							fontFamily: "Light",
+							marginBottom: "20px",
+							backgroundColor: "#363740",
+							borderRadius: "10px",
+						}}
+					>
+						Submit
+					</Button>
+				</div>
+			</div>
+		</Container>
+	)
 }
 
 export default Medicine
--- src/components/patient/patient.tsx
+++ src/components/patient/patient.tsx
@@ -1,7 +1,158 @@
-import * as React from "react"
+import { Button, Container, TextField } from "@mui/material"
+import React, { useState } from "react"
+import styles from "./styles.module.css"
 
 function Patient(props: any) {
-	return <span>patient funcional component</span>
+	const [name, setName] = useState()
+	const [lastName, setLastName] = useState()
+	const [gender, setGender] = useState()
+	const [age, setAge] = useState()
+	const [birthday, setBirthday] = useState()
+	const [bloodType, setBloodType] = useState()
+	const [diseaseBackground, setDiseaseBackground] = useState()
+	const [identificationId, setIdentificationId] = useState()
+	const [phoneNumber, setPhoneNumber] = useState()
+
+	function handleChangeName(event: any) {
+		setName(event.target.value)
+	}
+
+	function handleChangeLastName(event: any) {
+		setLastName(event.target.value)
+	}
+
+	function handleChangeGender(event: any) {
+		setGender(event.target.value)
+	}
+
+	function handleChangeAge(event: any) {
+		setAge(event.target.value)
+	}
+
+	function handleChangeBirthday(event: any) {
+		setBirthday(event.target.value)
+	}
+
+	function handleChangeBloodType(event: any) {
+		setBloodType(event.target.value)
+	}
+
+	function handleChangeDiseaseBackground(event: any) {
+		setDiseaseBackground(event.target.value)
+	}
+
+	function handlgeChangeIdentificationId(event: any) {
+		setIdentificationId(event.target.value)
+	}
+
+	function handleChangePhoneNumber(event: any) {
+		setPhoneNumber(event.target.value)
+	}
+
+	function createPatient() {}
+	return (
+		<Container fixed sx={{ bgcolor: "#FFFFFF", height: "80vh" }}>
+			<div className={styles.makeToColumn}>
+				<div className={styles.makeToRow}>
+					<TextField
+						required
+						value={name}
+						onChange={handleChangeName}
+						id="outlined-basic"
+						label="name"
+						variant="outlined"
+					/>
+					<TextField
+						required
+						value={gender}
+						onChange={handleChangeGender}
+						id="outlined-basic"
+						label="gender"
+						variant="outlined"
+					/>
+				</div>
+				<div className={styles.makeToRow}>
+					<TextField
+						required
+						value={lastName}
+						onChange={handleChangeLastName}
+						id="outlined-basic"
+						label="last name"
+						variant="outlined"
+					/>
+					<TextField
+						required
+						value={birthday}
+						onChange={handleChangeBirthday}
+						id="outlined-basic"
+						label="birthday"
+						variant="outlined"
+					/>
+				</div>
+				<div className={styles.makeToRow}>
+					<TextField
+						required
+						value={age}
+						onChange={handleChangeAge}
+						id="outlined-basic"
+						label="age"
+						variant="outlined"
+					/>
+					<TextField
+						required
+						value={bloodType}
+						onChange={handleChangeBloodType}
+						id="outlined-basic"
+						label="blood Type"
+						variant="outlined"
+					/>
+				</div>
+				<div className={styles.makeToRow}>
+					<TextField
+						required
+						value={diseaseBackground}
+						onChange={handleChangeDiseaseBackground}
+						id="outlined-basic"
+						label="disease Background"
+						variant="outlined"
+					/>
+					<TextField
+						required
+						value={identificationId}
+						onChange={handlgeChangeIdentificationId}
+						id="outlined-basic"
+						label="identification Id"
+						variant="outlined"
+					/>
+				</div>
+				<div className={styles.makeToRowSingle}>
+					<TextField
+						required
+						value={phoneNumber}
+						onChange={handleChangePhoneNumber}
+						id="outlined-basic"
+						label="phone Number"
+						variant="outlined"
+					/>
+				</div>
+				<div className={styles.buttonDiv}>
+					<Button
+						variant="contained"
+						onClick={createPatient}
+						type="submit"
+						sx={{
+							fontFamily: "Light",
+							marginBottom: "20px",
+							backgroundColor: "#363740",
+							borderRadius: "10px",
+						}}
+					>
+						Submit
+					</Button>
+				</div>
+			</div>
+		</Container>
+	)
 }
 
 export default Patient
