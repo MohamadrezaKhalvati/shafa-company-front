9c76bfa06e1f946ef729c4574a79c37a4b4e000f
mohamadreza <mohamadrezakhf@Gmail.com>
2023-01-26 12:37:41 +0330

    [feature] company and pharmacy component

--- src/components/device/device.tsx
+++ src/components/device/device.tsx
@@ -1,14 +1,12 @@
-import * as React from "react"
+import { Typography } from "@mui/material"
+import Paper from "@mui/material/Paper"
 import Table from "@mui/material/Table"
 import TableBody from "@mui/material/TableBody"
 import TableCell from "@mui/material/TableCell"
 import TableContainer from "@mui/material/TableContainer"
 import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
-import Paper from "@mui/material/Paper"
-import Card from "@mui/material/Card"
-import { Typography } from "@mui/material"
-
+import React from "react"
 function createData(name: string, calories: number, fat: number) {
 	return { name, calories, fat }
 }
